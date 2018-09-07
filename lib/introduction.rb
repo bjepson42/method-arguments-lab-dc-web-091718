8ad551c4b2b36f4eeefc1b3b006cfd44535c70a1
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 14:52:17 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,11 @@
+name = "josh"
+
 def introduction (name)
   put "Hi, my name is #{name}.\n"
 end
+
+name = "Dan"
+name = "Ember.js"
 
 def introduction_with_language(name,language)
   put "Hi, my name is #{name} and I am learning to program in #{language}.\n"
