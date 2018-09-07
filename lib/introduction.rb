de47974c491374e71e5da5dc611e0c472b1daafe
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 14:46:37 +0000

    automatically backed up by learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction ("Josh")
+  put "Hi, my name is #{name}.\n"
+end
+
+def introduction_with_language("Josh","Erlang.js")
+  put "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
